e5eed43ab6f6787960025e1aaf0c66492f38f3a7
Hugo Falcao <hugocoelhof03@gmail.com>
2024-04-04 12:24:29 -0300

    feat: :card_file_box: Criar migrations

---
+++ WakeChallenge/WakeChallenge.INFRASTRUCTURE/Migrations/20240404020405_initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WakeChallenge.INFRASTRUCTURE.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Stock = table.Column<int>(type: "integer", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "ProductId", "Name", "Stock", "Value" },
+                values: new object[,]
+                {
+                    { 1, "Refrigerante X", 100, 10.90m },
+                    { 2, "Bolo de cenoura", 5, 25.99m },
+                    { 3, "Leite condensado", 23, 7.60m },
+                    { 4, "Creme de leite", 45, 3.25m },
+                    { 5, "Barra de chocolate", 111, 10.33m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_Name",
+                table: "products",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
