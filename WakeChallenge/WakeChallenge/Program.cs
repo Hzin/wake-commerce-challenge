20e73386d7e28986470c2968fc602ff96248ce9f
Hugo Falcao <hugocoelhof03@gmail.com>
2024-04-04 12:18:34 -0300

    feat: :sparkles: Adicionar ponto inicial e de configurações do projeto

---
+++ WakeChallenge/WakeChallenge/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WakeChallenge.INFRASTRUCTURE.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connection to postgres
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql("Server=localhost;Port=5432;User Id=postgres;Password=Admin@123;Database=wakechallenge;");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
